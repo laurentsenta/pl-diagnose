6cd9eab19a219dac848b32f639d2cc05b3dc18cf
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-01-23 13:29:21 -0800

    feat: add telemetry

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,25 +1,34 @@
-import { CommonParamsProvider } from "data/useCommonParams";
-import React from "react";
-import ReactDOM from "react-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { HashRouter } from "react-router-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "./style.scss";
+import { CommonParamsProvider } from 'data/useCommonParams';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './style.scss';
 
 export const queryClient = new QueryClient();
 
+(async () => {
+    const { BrowserMetricsProvider } = await import('@ipfs-shipyard/ignite-metrics/browser-vanilla');
+    const telemetry = new BrowserMetricsProvider({ appKey: 'cd159bec282a4daeac0e295f308e8618448ec526' });
+
+    window.telemetry = telemetry;
+    window.removeMetricsConsent = () => telemetry.removeConsent(['minimal']);
+    window.addMetricsConsent = () => telemetry.addConsent(['minimal']);
+})();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <HashRouter>
-        <CommonParamsProvider>
-          <App />
-        </CommonParamsProvider>
-      </HashRouter>
-    </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <HashRouter>
+                <CommonParamsProvider>
+                    <App />
+                </CommonParamsProvider>
+            </HashRouter>
+        </QueryClientProvider>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
