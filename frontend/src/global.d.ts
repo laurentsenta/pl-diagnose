d40917a351e885729594ba7f4930a7cb546012ce
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-01-23 16:15:08 -0800

    chore: revert style changes
    had to disable pre-commit hooks which change styles to keep my diff
    appropriate.

--- frontend/src/global.d.ts
+++ frontend/src/global.d.ts
@@ -1,7 +1,7 @@
 import { BrowserMetricsProvider } from '@ipfs-shipyard/ignite-metrics';
 
 declare global {
-    var telemetry: InstanceType<BrowserMetricsProvider>;
-    var addMetricsConsent: () => void;
-    var removeMetricsConsent: () => void;
+  var telemetry: InstanceType<BrowserMetricsProvider>;
+  var addMetricsConsent: () => void;
+  var removeMetricsConsent: () => void;
 }
